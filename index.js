388b715f22bcebd10cbe5dbed7639f6ef77b7b01
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-11-05 16:58:56 -0700

    updates to js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
 
-let firstCard = Math.floor(Math.random() * 10)
-let secondCard = Math.floor(Math.random() * 10)
+let firstCard = Math.floor(Math.random() * 10) + 2
+console.log(firstCard)
+let secondCard = Math.floor(Math.random() * 10) + 2
+console.log(secondCard)
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard 
+console.log(sum)
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?")
+
+} else if (sum === 21) {
+     console.log("Wohoo! You've got Blackjack!")
+
+} else {
+     console.log("You're out of the game!")
+
+}
